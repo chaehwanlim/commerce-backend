13c0833dfa45c40fc77d308162866305c20c4721
Chaehwan Lim <chlim428@gmail.com>
2024-07-11 15:14:34 +0900

    initialize project

---
+++ src/main/java/chlim/commercebackend/CommercebackendApplication.java
@@ -0,0 +1,13 @@
+package chlim.commercebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommercebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommercebackendApplication.class, args);
+	}
+
+}
