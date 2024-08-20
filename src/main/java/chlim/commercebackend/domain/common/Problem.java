af5aa5f869c9f74ac8643b3e4d243d63574efc62
Chaehwan Lim <chlim428@gmail.com>
2024-08-20 13:29:25 +0900

    feat: 실제 exception을 포함하는 problem이 더 자세한 에러 메시지를 표시하도록 개선

--- src/main/java/chlim/commercebackend/domain/common/Problem.java
+++ src/main/java/chlim/commercebackend/domain/common/Problem.java
@@ -1,16 +1,28 @@
 package chlim.commercebackend.domain.common;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
 @Getter
 @Builder
-@AllArgsConstructor
 public class Problem extends RuntimeException {
 
 	private final ProblemCategory category;
 	private final String errorCode;
 	private final String message;
 	private final Exception cause;
+
+	public Problem(ProblemCategory category, String errorCode, String message, Exception cause) {
+		super(message, cause);
+
+		this.category = category;
+		this.errorCode = errorCode;
+		if (cause != null && cause.getMessage() != null) {
+			this.message = String.format("%s (cause: %s)", message, cause.getMessage());
+		} else {
+			this.message = message;
+		}
+
+		this.cause = cause;
+	}
 }
